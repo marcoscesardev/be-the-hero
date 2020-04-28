fa425e3ad9c87d55809b723ab5219439a8d49f0d
Marcos César <marcoscesar.dev@gmail.com>
2020-04-27 22:02:49 -0300

    feat: Add session controller

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -2,12 +2,12 @@ const crypto = require('crypto');
 const connection = require('../database/connection');
 
 module.exports = {
-  async index (request, response) {
+  async index(request, response) {
     const ongs = await connection('ongs').select('*');
 
     return response.json(ongs);
   },
-  async create (request, response) {
+  async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
 
     const id = crypto.randomBytes(4).toString('HEX');
